eda7134094613a5da92416a903d603bba339ad29
JakeRYW <JacobWeeks11@Gmail.com>
2024-05-31 15:51:06 -0400

    Updating Dark Mode compatability

--- src/app/(app)/drivers/page.tsx
+++ src/app/(app)/drivers/page.tsx
@@ -1,6 +1,7 @@
 import { getStocks } from '@/app/actions/actions';
 import { columns } from './components/columns';
 import { DataTable } from './components/data-table';
+import { Card, CardContent } from '@/components/ui/card';
 
 export default async function DriversPage() {
 	const stocks = await getStocks();
@@ -11,12 +12,16 @@ export default async function DriversPage() {
 				<div className='mt-10 flex items-center justify-center flex-col'>
 					<h2 className='text-5xl font-semibold'>Driver List</h2>
 					<div className='container mx-auto py-10'>
-						<div className='bg-white rounded-lg shadow-md outline outline-1 outline-[#e4e4e7]'>
+						<div className='bg-background rounded-lg shadow-md outline outline-1 outline-border'>
 							<div className='p-5'>
-								<DataTable
-									columns={columns}
-									data={stocks ? stocks : []}
-								/>
+								<Card>
+									<CardContent>
+										<DataTable
+											columns={columns}
+											data={stocks ? stocks : []}
+										/>
+									</CardContent>
+								</Card>
 							</div>
 						</div>
 					</div>
