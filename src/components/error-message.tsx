2eb06b324a6d5057e5667d810a2a40147f52ccc3
JakeRYW <JacobWeeks11@Gmail.com>
2024-07-04 10:47:32 -0400

    Adjusting error alert color in dark mode

--- src/components/error-message.tsx
+++ src/components/error-message.tsx
@@ -6,8 +6,11 @@ import { AlertCircle } from 'lucide-react';
 export default function ErrorMessage() {
 	return (
 		<div className='w-full'>
-			<Alert variant='destructive'>
-				<AlertCircle className='h-4 w-4' />
+			<Alert
+				variant='destructive'
+				className='dark:text-red-700 dark:!border-red-700'
+			>
+				<AlertCircle className='h-4 w-4 dark:!text-red-700' />
 				<AlertTitle>Error</AlertTitle>
 				<AlertDescription>
 					No stocks found. Please try again later.
