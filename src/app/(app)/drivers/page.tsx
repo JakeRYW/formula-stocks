09ec4e2b142e723b1329b37cce6e49c91444f3ac
JakeRYW <JacobWeeks11@Gmail.com>
2024-07-05 19:18:50 -0400

    Adding cards to components

--- src/app/(app)/drivers/page.tsx
+++ src/app/(app)/drivers/page.tsx
@@ -12,18 +12,10 @@ export default async function DriversPage() {
 				<div className='mt-10 flex items-center justify-center flex-col'>
 					<h2 className='text-5xl font-semibold'>Driver List</h2>
 					<div className='container mx-auto py-10'>
-						<div className='bg-background rounded-lg shadow-md outline outline-1 outline-border'>
-							<div className='p-5'>
-								<Card>
-									<CardContent>
-										<DataTable
-											columns={columns}
-											data={stocks ? stocks : []}
-										/>
-									</CardContent>
-								</Card>
-							</div>
-						</div>
+						<DataTable
+							columns={columns}
+							data={stocks ? stocks : []}
+						/>
 					</div>
 				</div>
 			</main>
