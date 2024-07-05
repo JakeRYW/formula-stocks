e1ffffeefca8a7cae6a4e92bb4e85b87a70fcefb
JakeRYW <JacobWeeks11@Gmail.com>
2024-07-05 19:18:15 -0400

    Updating leaderboard functionality

--- src/app/(app)/leaderboards/components/columns.tsx
+++ src/app/(app)/leaderboards/components/columns.tsx
@@ -5,7 +5,39 @@ import { ColumnDef } from '@tanstack/react-table';
 
 export const columns: ColumnDef<Stock>[] = [
 	{
+		header: 'Rank',
+		cell: ({ row }) => {
+			return <div className=''>{row.index + 1}</div>;
+		},
+	},
+	{
 		accessorKey: 'name',
 		header: 'Username',
+	},
+	{
+		accessorKey: 'netWorth',
+		header: 'Net Worth',
+		cell: ({ row }) => {
+			const amount = parseFloat(row.getValue('netWorth'));
+			const formatted = new Intl.NumberFormat('en-US', {
+				style: 'currency',
+				currency: 'USD',
+			}).format(amount);
+
+			return <div>{formatted}</div>;
+		},
+	},
+	{
+		accessorKey: 'portfolioSize',
+		header: 'Portfolio Size',
+		cell: ({ row }) => {
+			return (
+				<div>{`${row.getValue('portfolioSize')} ${
+					Number(row.getValue('portfolioSize')) > 1
+						? 'stocks'
+						: 'stock'
+				}`}</div>
+			);
+		},
 	},
 ];
--- src/app/(app)/leaderboards/page.tsx
+++ src/app/(app)/leaderboards/page.tsx
@@ -1,16 +1,16 @@
-import { getLeaderboardResults } from '@/app/actions/actions';
+import { getCachedLeaderboardResults } from '@/app/actions/actions';
 import { columns } from './components/columns';
 import { DataTable } from './components/data-table';
 
 export default async function LeaderboardsPage() {
-	const leaderboardData = await getLeaderboardResults();
+	const leaderboardData = await getCachedLeaderboardResults();
 
 	return (
 		<>
 			<main className='flex flex-col gap-4 p-4 lg:gap-6 lg:p-6'>
 				<div className='mt-10 flex items-center justify-center flex-col'>
 					<h2 className='text-5xl font-semibold'>Leaderboards</h2>
-					<div className='container mx-auto py-10'>
+					<div className='container mx-auto py-10 '>
 						<DataTable
 							columns={columns}
 							data={leaderboardData ? leaderboardData : []}
