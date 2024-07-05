962ed3e9d811707939549d1d0893e45e991461eb
JakeRYW <JacobWeeks11@Gmail.com>
2024-07-05 19:21:27 -0400

    Refactor data fetching to actions

--- src/app/(app)/stock/[username]/page.tsx
+++ src/app/(app)/stock/[username]/page.tsx
@@ -2,30 +2,18 @@ import StockCard from './components/stock-card';
 import OrderCard from './components/order-card';
 
 import { auth } from '@/lib/auth';
-import { getBalanceById, getStockQuantity } from '@/app/actions/actions';
+import {
+	getBalanceById,
+	getStockByName,
+	getStockQuantity,
+} from '@/app/actions/actions';
 
 import { Toaster } from '@/components/ui/toaster';
-
-async function getStock(username: string) {
-	try {
-		const res = await fetch(
-			`${process.env.API_BASE}/api/stocks/${username}`,
-			{
-				cache: 'no-cache',
-			}
-		);
-		if (!res.ok) throw new Error('Failed to fetch stocks');
-		return res.json();
-	} catch (error) {
-		console.error('Failed to fetch stocks from the server!', error);
-		return null;
-	}
-}
 
 async function getChangeData(username: string) {
 	try {
 		const res = await fetch(
-			`${process.env.API_BASE}/api/stocks/${username}/history`,
+			`http://localhost:5000/api/stocks/${username}/history`,
 			{ cache: 'no-cache' }
 		);
 		if (!res.ok) throw new Error('Failed to fetch change data');
@@ -42,23 +30,23 @@ export default async function StockPage({
 	params: { username: string };
 }) {
 	const session = await auth();
-	const stock = await getStock(params.username);
+	const stock = await getStockByName(params.username.replace('-', ' '));
 	const changeData = await getChangeData(params.username);
 
 	const balance = session ? await getBalanceById(session?.user.id) : 0;
-	const stockQuantity = session ? await getStockQuantity(stock[0].id) : 0;
+	const stockQuantity = session ? await getStockQuantity(stock.id) : 0;
 
 	return (
 		<>
 			<div className='flex flex-row justify-center mt-10 mb-20'>
 				<StockCard
-					stock={stock[0]}
+					stock={stock}
 					changeData={changeData !== null ? changeData[0] : null}
 				/>
 				{session && balance !== null && stockQuantity !== null ? (
 					<div className='ml-5'>
 						<OrderCard
-							stock={stock[0]}
+							stock={stock}
 							balance={Number(balance)}
 							quantity={stockQuantity}
 						/>
@@ -66,8 +54,8 @@ export default async function StockPage({
 				) : (
 					''
 				)}
+				<Toaster />
 			</div>
-			<Toaster />
 		</>
 	);
 }
