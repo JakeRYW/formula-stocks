9bd106a19f066e2d0e91ab28c52af1dad8cb2a4d
JakeRYW <JacobWeeks11@Gmail.com>
2024-07-05 19:16:07 -0400

    Adding better error handling to actions

--- src/app/actions/actions.ts
+++ src/app/actions/actions.ts
@@ -2,9 +2,23 @@
 
 import { db } from '@/lib/db';
 import { portfolio, stocks, transactions, users } from '@/drizzle/schema';
-import { and, asc, desc, eq, ilike, sql } from 'drizzle-orm';
+import { and, desc, eq, ilike, sql } from 'drizzle-orm';
 import { auth } from '@/lib/auth';
 import { TradeOptions } from '@/types';
+import { unstable_cache } from 'next/cache';
+
+const RED = '\u001b[31m';
+const BLUE = '\u001b[34m';
+const WHITE = '\u001b[37m';
+
+const REDBACKGROUND = '\u001b[37;41m';
+const WHITEBACKGROUND = '\u001b[37;47m';
+
+const BOLD = '\u001b[1m';
+const ITALICIZE = '\u001b[3m';
+const UNDERLINE = '\u001b[4m';
+
+const RESET = '\u001b[0m';
 
 export async function fetchBalance(username: String) {
 	const result = await db
@@ -25,9 +39,30 @@ export async function getStocks() {
 			.orderBy(stocks.championship_pos);
 		return allStocks;
 	} catch (error) {
-		console.error(error);
-		return null;
+		switch (error.code) {
+			case 'ECONNREFUSED':
+				console.error(
+					RED + BOLD + UNDERLINE + 'Error: Database' + RESET
+				);
+				console.error(
+					'ECONNREFUSED Error trying to acccess database in getStocks'
+				);
+				break;
+			default:
+				console.error(`Error in getStocks: ${error}`);
+				break;
+		}
+		return [];
 	}
+}
+
+export async function getStockByName(stockName: String) {
+	const result = await db
+		.select()
+		.from(stocks)
+		.where(ilike(stocks.name, stockName));
+
+	return result[0];
 }
 
 export async function getBalanceById(userId: String) {
@@ -38,9 +73,36 @@ export async function getBalanceById(userId: String) {
 		.from(users)
 		.where(eq(users.id, userId));
 
-	console.log(result[0]);
-
 	return result[0].balance;
+}
+
+export async function getUserBalance() {
+	const session = await auth();
+
+	if (!session || !session.user || !session.user.id) return null;
+
+	try {
+		const account = await db
+			.select()
+			.from(users)
+			.where(eq(users.id, session.user.id));
+
+		return account[0].balance;
+	} catch (error) {
+		switch (error.code) {
+			case 'ECONNREFUSED':
+				console.error(
+					RED + BOLD + UNDERLINE + 'Error: Database' + RESET
+				);
+				console.error(
+					'ECONNREFUSED Error trying to access database in getUserBalance'
+				);
+				break;
+			default:
+				console.error(`Error in getUserBalance: ${error}`);
+				break;
+		}
+	}
 }
 
 export async function manualTransaction(username: string, amount: number) {
@@ -65,7 +127,10 @@ export async function transaction(
 		if (!session || !session.user || !session.user.id) throw Error;
 
 		console.log(
-			`User ${session?.user.name} submitted ${type} order for ${amount} shares of stock: ${stockId}`
+			BLUE +
+				'Transaction Record: ' +
+				RESET +
+				`User ${session?.user.name} submitted ${type} order for ${amount} shares of stock: ${stockId}`
 		);
 
 		const transactionRecord = await db.insert(transactions).values({
@@ -75,10 +140,19 @@ export async function transaction(
 			quantity: amount,
 		});
 
+		const balanceResult = await db
+			.select({ balance: users.balance })
+			.from(users)
+			.where(eq(users.id, session.user.id));
+
+		const portfolioValueResult = await getPortfolioValue();
+
 		return {
 			success: `You have successfully ${
 				type === 'buy' ? 'bought' : 'sold'
 			} ${amount} ${amount > 1 ? 'shares!' : 'share!'}`,
+			balance: balanceResult[0].balance,
+			portfolioValue: portfolioValueResult,
 		};
 	} catch (error) {
 		console.error('Error performing transaction');
@@ -94,21 +168,38 @@ export async function getStockQuantity(stockId: string) {
 
 	if (!session || !session.user || !session.user.id) return null;
 
-	const portfolioResult = await db
-		.select()
-		.from(portfolio)
-		.where(
-			and(
-				eq(portfolio.userId, session.user.id),
-				eq(portfolio.stockId, stockId)
-			)
-		);
+	try {
+		const portfolioResult = await db
+			.select()
+			.from(portfolio)
+			.where(
+				and(
+					eq(portfolio.userId, session.user.id),
+					eq(portfolio.stockId, stockId)
+				)
+			);
 
-	if (portfolioResult.length === 0) {
+		if (portfolioResult.length === 0) {
+			return 0;
+		}
+
+		return portfolioResult[0].quantity;
+	} catch (error) {
+		switch (error.code) {
+			case 'ECONNREFUSED':
+				console.error(
+					RED + BOLD + UNDERLINE + 'Error: Database' + RESET
+				);
+				console.error(
+					'ECONNREFUSED Error trying to access database in getStockQuantity'
+				);
+				break;
+			default:
+				console.error(`Error in getStockQuantity: ${error}`);
+				break;
+		}
 		return 0;
 	}
-
-	return portfolioResult[0].quantity;
 }
 
 export async function getPortfolioValue() {
@@ -116,27 +207,44 @@ export async function getPortfolioValue() {
 
 	if (!session || !session.user || !session.user.id) return null;
 
-	const portfolioResult = await db
-		.select({
-			quantity: portfolio.quantity,
-			price: stocks.price,
-		})
-		.from(portfolio)
-		.innerJoin(
-			stocks,
-			and(
-				eq(portfolio.stockId, stocks.id),
-				eq(portfolio.userId, session.user.id)
-			)
-		);
+	try {
+		const portfolioResult = await db
+			.select({
+				quantity: portfolio.quantity,
+				price: stocks.price,
+			})
+			.from(portfolio)
+			.innerJoin(
+				stocks,
+				and(
+					eq(portfolio.stockId, stocks.id),
+					eq(portfolio.userId, session.user.id)
+				)
+			);
 
-	let totalAmount = 0;
+		let totalAmount = 0;
 
-	for (let stock of portfolioResult) {
-		totalAmount += Number(stock.price) * Number(stock.quantity);
+		for (let stock of portfolioResult) {
+			totalAmount += Number(stock.price) * Number(stock.quantity);
+		}
+
+		return Math.round(totalAmount * 100) / 100;
+	} catch (error) {
+		switch (error.code) {
+			case 'ECONNREFUSED':
+				console.error(
+					RED + BOLD + UNDERLINE + 'Error: Database' + RESET
+				);
+				console.error(
+					'ECONNREFUSED Error trying to access database in getPortfolioValue'
+				);
+				break;
+			default:
+				console.error(`Error in getPortfolioValue: ${error}`);
+				break;
+		}
+		return 0;
 	}
-
-	return Math.round(totalAmount * 100) / 100;
 }
 
 export async function setBalance(username: string, amount: number) {
@@ -161,63 +269,86 @@ export async function getStockPrice(stockId: string) {
 	return result[0].price;
 }
 
-export async function getLeaderboardResults() {
-	const usersResult = await db
-		.select({
-			name: users.name,
-			stockId: portfolio.stockId,
-			quantity: portfolio.quantity,
-			//portfolioCount: sql<number>`cast(count(${portfolio.userId}) as int)`,
-			price: stocks.price,
-			totalPrice: sql<number>`sum(${portfolio.quantity} * ${stocks.price})`,
-		})
-		.from(users)
-		.leftJoin(portfolio, eq(users.id, portfolio.userId))
-		.leftJoin(stocks, eq(portfolio.stockId, stocks.id))
-		.groupBy(
-			users.name,
-			portfolio.stockId,
-			portfolio.quantity,
-			users.balance,
-			stocks.price
-		)
-		.orderBy(asc(users.balance));
+export const getCachedLeaderboardResults = unstable_cache(
+	async () => getLeaderboardResults(10),
+	['leaderboard-results'],
+	{ revalidate: 1 }
+);
 
-	console.log(usersResult);
+export async function getLeaderboardResults(limit: number) {
+	try {
+		const leaderboardResults = await db
+			.select({
+				name: users.name,
+				netWorth: sql<number>`${users.balance} + COALESCE(sum(${portfolio.quantity} * ${stocks.price}), 0)`,
+				portfolioSize: sql<number>`count(${portfolio.userId})`,
+			})
+			.from(users)
+			.leftJoin(portfolio, eq(users.id, portfolio.userId))
+			.leftJoin(stocks, eq(portfolio.stockId, stocks.id))
+			.groupBy(users.name, users.balance)
+			.orderBy(desc(sql<number>`count(${portfolio.userId})`))
+			.limit(limit);
 
-	// const combined = usersResult.reduce((acc, { name, portfolio }) => {
-	// 	if (!acc[name]) {
-	// 		acc[name] = { name, portfolio: [] };
-	// 	}
-	// 	acc[name].portfolio.push(portfolio);
-	// 	return acc;
-	// }, {});
-
-	return [];
+		return leaderboardResults;
+	} catch (error) {
+		switch (error.code) {
+			case 'ECONNREFUSED':
+				console.error(
+					RED + BOLD + UNDERLINE + 'Error: Database' + RESET
+				);
+				console.error(
+					'ECONNREFUSED Error trying to acccess database in getLeaderboardResults'
+				);
+				break;
+			default:
+				console.error(`Error in getLeaderboardResults: ${error}`);
+				break;
+		}
+		return null;
+	}
 }
 
 export async function getRecentTransactions(limit: number) {
-	const transactionsResult = await db
-		.select({
-			name: users.name,
-			image: users.image,
-			symbol: stocks.symbol,
-			type: transactions.transaction_type,
-			quantity: transactions.quantity,
-			totalPrice: sql<number>`sum(${transactions.total_price} * ${transactions.quantity})`,
-		})
-		.from(transactions)
-		.innerJoin(users, eq(users.id, transactions.userId))
-		.innerJoin(stocks, eq(stocks.id, transactions.stockId))
-		.groupBy(
-			users.name,
-			users.image,
-			transactions.transaction_type,
-			transactions.quantity,
-			stocks.symbol,
-			transactions.created_at
-		)
-		.orderBy(desc(transactions.created_at))
-		.limit(limit);
-	return transactionsResult;
+	try {
+		const transactionsResult = await db
+			.select({
+				name: users.name,
+				image: users.image,
+				symbol: stocks.symbol,
+				type: transactions.transaction_type,
+				quantity: transactions.quantity,
+				totalPrice: transactions.total_price,
+			})
+			.from(transactions)
+			.innerJoin(users, eq(users.id, transactions.userId))
+			.innerJoin(stocks, eq(stocks.id, transactions.stockId))
+			.groupBy(
+				users.name,
+				users.image,
+				transactions.transaction_type,
+				transactions.quantity,
+				stocks.symbol,
+				transactions.created_at,
+				transactions.total_price
+			)
+			.orderBy(desc(transactions.created_at))
+			.limit(limit);
+		return transactionsResult;
+	} catch (error) {
+		switch (error.code) {
+			case 'ECONNREFUSED':
+				console.error(
+					RED + BOLD + UNDERLINE + 'Error: Database' + RESET
+				);
+				console.error(
+					'ECONNREFUSED Error trying to acccess database in getPortfolioValue'
+				);
+				break;
+			default:
+				console.error(`Error in getPortfolioValue: ${error}`);
+				break;
+		}
+		return [];
+	}
 }
