81408ab0e97f7477e78f1780afa4664e07fa599b
JakeRYW <JacobWeeks11@Gmail.com>
2024-08-10 19:04:18 -0400

    Pass history to chart

--- src/app/(app)/stock/[username]/page.tsx
+++ src/app/(app)/stock/[username]/page.tsx
@@ -5,24 +5,15 @@ import { auth } from '@/lib/auth';
 import {
 	getBalanceById,
 	getStockByName,
+	getStockHistoryAll,
+	getStockHistoryDay,
+	getStockHistoryHour,
+	getStockHistoryMonth,
+	getStockHistoryWeek,
 	getStockQuantity,
 } from '@/app/actions/actions';
 
 import { Toaster } from '@/components/ui/toaster';
-
-async function getChangeData(username: string) {
-	try {
-		const res = await fetch(
-			`http://localhost:5000/api/stocks/${username}/history`,
-			{ cache: 'no-cache' }
-		);
-		if (!res.ok) throw new Error('Failed to fetch change data');
-		return res.json();
-	} catch (error) {
-		console.error('Failed to fetch change data from the server!', error);
-		return null;
-	}
-}
 
 export default async function StockPage({
 	params,
@@ -31,18 +22,46 @@ export default async function StockPage({
 }) {
 	const session = await auth();
 	const stock = await getStockByName(params.username.replace('-', ' '));
-	const changeData = await getChangeData(params.username);
-
 	const balance = session ? await getBalanceById(session?.user.id) : 0;
 	const stockQuantity = session ? await getStockQuantity(stock.id) : 0;
+
+	const hourStockHistory = await getStockHistoryHour(
+		params.username.replace('-', ' ')
+	);
+	const dayStockHistory = await getStockHistoryDay(
+		params.username.replace('-', ' ')
+	);
+	const weekStockHistory = await getStockHistoryWeek(
+		params.username.replace('-', ' ')
+	);
+	const monthStockHistory = await getStockHistoryMonth(
+		params.username.replace('-', ' ')
+	);
+	const allStockHistory = await getStockHistoryAll(
+		params.username.replace('-', ' ')
+	);
+
+	const downsampleData = (data, requiredPoints) => {
+		if (data.length <= requiredPoints) {
+			return data;
+		}
+		const valueCount = data.length;
+		const step = Math.floor(valueCount / requiredPoints);
+		return data.filter((_: {}, index: number) => index % step === 0);
+	};
+
+	const dummyData = {
+		hour: downsampleData(hourStockHistory, 80),
+		day: downsampleData(dayStockHistory, 80),
+		week: downsampleData(weekStockHistory, 80),
+		month: downsampleData(monthStockHistory, 80),
+		all: downsampleData(allStockHistory, 80),
+	};
 
 	return (
 		<>
 			<div className='flex flex-row justify-center mt-10'>
-				<StockCard
-					stock={stock}
-					changeData={changeData !== null ? changeData[0] : null}
-				/>
+				<StockCard stock={stock} changeData={dummyData} />
 				{session && balance !== null && stockQuantity !== null ? (
 					<div className='ml-5'>
 						<OrderCard
