5b836b976764cac4ac1fcdd8601aea49b83a80bc
JakeRYW <JacobWeeks11@Gmail.com>
2024-08-26 19:29:13 -0400

    Adding randomized facts and loaders

--- src/components/track-loading.tsx
+++ src/components/track-loading.tsx
@@ -3,28 +3,23 @@ import MonzaLoader from './loading-spinner/tracks/monza';
 import RedbullLoader from './loading-spinner/tracks/redbull';
 
 export default function TrackLoadingSpinner() {
-	// let track = Math.random() * (3 - 0) + 0;
-	// let fact = Math.random() * (2 - 0) + 0;
+	const facts = [
+		'Jenson Button recieved a 70-place grid penalty at the 2015 Mexican GP',
+		'Lewis Hamilton has won the most races in the history of the F1 World Championship',
+		'Brawn GP won every WDC and WCC they competed in',
+		'A modern F1 car is made up of 80,000 components',
+	];
 
-	// switch (track) {
-	// 	case 0: {
-	// 		return <></>;
-	// 	}
-	// 	case 1: {
-	// 		return <></>;
-	// 	}
-	// }
+	const loaders = [HungaroringLoader, MonzaLoader, RedbullLoader];
+	const RandomLoader = loaders[Math.floor(Math.random() * loaders.length)];
 
 	return (
 		<div className='flex flex-col justify-center'>
-			{/* <HungaroringLoader /> */}
-			{/* <RedbullLoader /> */}
-			<MonzaLoader />
+			<RandomLoader />
 
 			<div className='flex justify-center'>
 				<p className='font-semibold opacity-35 text-center text-xs -mt-5 max-w-56'>
-					Jenson Button recieved a 70-place grid penalty at the 2015
-					Mexican GP
+					{facts[Math.floor(Math.random() * facts.length)]}
 				</p>
 			</div>
 		</div>
