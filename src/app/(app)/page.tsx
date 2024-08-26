35d9042639662bdec01c60750e6c4c866e71d5b2
JakeRYW <JacobWeeks11@Gmail.com>
2024-08-26 19:27:20 -0400

    Updating homepage stock card responsiveness

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,38 +1,25 @@
 import ErrorMessage from '@/components/error-message';
 import StockCard from '@/components/stock-card';
-
 import { getStocks } from '@/app/actions/actions';
-import { Stock } from '@/types';
 
 export default async function HomePage() {
 	const stocks = await getStocks();
 
 	return (
-		<>
-			<div className='flex flex-col'>
-				<main className='flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6'>
-					<div className='flex items-center flex-col'>
-						{!stocks || stocks.length === 0 ? (
-							<div className='mt-5 flex items-center justify-center'>
-								<div>
-									<ErrorMessage />
-								</div>
-							</div>
-						) : (
-							<div className='flex flex-wrap justify-center mx-4 my-8 overflow-hidden'>
-								{stocks.map((stock: Stock) => (
-									<div className='p-3' key={stock.id}>
-										<StockCard
-											key={stock.id}
-											stock={stock}
-										/>
-									</div>
-								))}
-							</div>
-						)}
+		<main className='flex flex-1 flex-col gap-4 p-4 md:gap-8 md:p-8'>
+			<div className='grid gap-4 md:grid-cols-1 md:gap-8 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-3 4xl:grid-cols-4 5xl:grid-cols-5'>
+				{!stocks || stocks.length === 0 ? (
+					<div className='mt-5 flex items-center justify-center'>
+						<div>
+							<ErrorMessage />
+						</div>
 					</div>
-				</main>
+				) : (
+					stocks.map((stock) => (
+						<StockCard key={stock.id} stock={stock} />
+					))
+				)}
 			</div>
-		</>
+		</main>
 	);
 }
--- src/components/stock-card.tsx
+++ src/components/stock-card.tsx
@@ -6,6 +6,17 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { addOrdinalSuffix, getPriceChange } from '@/lib/utils';
 import { Stock } from '@/types';
+import { useEffect, useState } from 'react';
+import { Button } from './ui/button';
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from './ui/dropdown-menu';
+import { DropdownMenuLabel } from '@radix-ui/react-dropdown-menu';
+import { Card, CardContent, CardHeader, CardTitle } from './ui/card';
 
 interface StockCardProps {
 	stock: Stock;
@@ -24,13 +35,19 @@ export default function StockCard({ stock }: StockCardProps) {
 		image,
 	} = stock;
 
+	const [prevPrice, setPrevPrice] = useState(stock.price);
+
+	useEffect(() => {
+		setPrevPrice(stock.price);
+	}, [stock.price]);
+
 	return (
-		<>
-			<div>
-				<div
-					className={`-z-50 py-5 px-8 text-black bg-[#ffffff] shadow-md rounded-lg w-[23.5rem] dark:bg-[#1b1b1b] dark:text-white border-l-[6px]`}
-					style={{ borderColor: `#${color}` }}
-				>
+		<Card
+			style={{ borderColor: `#${color}` }}
+			className='border-0 border-l-[6px]'
+		>
+			<CardHeader className='pb-0'>
+				<CardTitle>
 					<div className='flex items-center '>
 						<Image
 							className='w-[1.70rem]'
@@ -54,91 +71,112 @@ export default function StockCard({ stock }: StockCardProps) {
 								{symbol.toUpperCase()}
 							</p>
 						</Link>
-						<a
-							className='-z-0 ml-auto -mt-3 -mr-3 '
-							href={`#`}
-							target='_blank'
-							rel='noreferrer'
-						>
-							<MoreHorizontal />
-						</a>
+						<DropdownMenu>
+							<DropdownMenuTrigger className='-z-0 ml-auto -mt-3 -mr-3 hidden xs:block'>
+								<Button
+									variant='ghost'
+									className='hover:bg-transparent px-0 '
+									asChild
+								>
+									<MoreHorizontal />
+								</Button>
+							</DropdownMenuTrigger>
+							<DropdownMenuContent>
+								<DropdownMenuLabel>
+									Stock Options
+								</DropdownMenuLabel>
+								<DropdownMenuSeparator />
+								<DropdownMenuItem asChild>
+									<Link
+										className='flex flex-row items-center'
+										href={`stock/${name
+											.replace(' ', '-')
+											.toLowerCase()}`}
+									>
+										View more info
+									</Link>
+								</DropdownMenuItem>
+								<DropdownMenuItem>Buy/Sell</DropdownMenuItem>
+							</DropdownMenuContent>
+						</DropdownMenu>
 					</div>
-					<div className='flex flex-col'>
-						<p className='mt-3 text-lg font-semibold'>
-							$
-							{Number(price).toLocaleString(undefined, {
-								minimumFractionDigits: 2,
-								maximumFractionDigits: 2,
-							})}
-						</p>
+				</CardTitle>
+			</CardHeader>
+			<CardContent>
+				<div className='flex flex-col'>
+					<div className='mt-3 text-lg font-semibold'>
+						{`$${Number(price).toLocaleString(undefined, {
+							minimumFractionDigits: 2,
+							maximumFractionDigits: 2,
+						})}`}
+					</div>
 
-						<p
-							className={`font-semibold -mt-1 ${
-								Number(change_24hr) >= 0
-									? 'text-green-600'
-									: 'text-red-700'
-							}`}
-						>
-							{`${
-								Number(change_24hr) >= 0
-									? '$' +
-									  getPriceChange(
+					<p
+						className={`font-semibold -mt-1 ${
+							Number(change_24hr) >= 0
+								? 'text-green-600'
+								: 'text-red-700'
+						}`}
+					>
+						{`${
+							Number(change_24hr) >= 0
+								? '$' +
+								  getPriceChange(
+										Number(price),
+										Number(change_24hr)
+								  ).toFixed(2)
+								: '-$' +
+								  Math.abs(
+										getPriceChange(
 											Number(price),
 											Number(change_24hr)
-									  ).toFixed(2)
-									: '-$' +
-									  Math.abs(
-											getPriceChange(
-												Number(price),
-												Number(change_24hr)
-											)
-									  ).toFixed(2)
-							} (%${Math.abs(Number(change_24hr)).toFixed(2)})`}
-							<span className='text-sm text-black opacity-50 dark:text-white'>
-								{' past day'}
+										)
+								  ).toFixed(2)
+						} (%${Math.abs(Number(change_24hr)).toFixed(2)})`}
+						<span className='text-sm text-black opacity-50 dark:text-white'>
+							{' past day'}
+						</span>
+					</p>
+				</div>
+				<div className='flex flex-wrap gap-2 mt-5'>
+					<div className='px-4 py-1 rounded-full bg-slate-200 w-fit dark:bg-[#3b3b3b]'>
+						<div className='flex items-center'>
+							<IoPodiumOutline
+								className='mr-1 -ml-1'
+								size={16}
+								strokeWidth={20}
+							/>
+							<span className='font-semibold'>
+								{addOrdinalSuffix(Number(championship_pos))}
 							</span>
-						</p>
+						</div>
 					</div>
-					<div className='flex flex-row'>
-						<div className='px-4 py-1 mt-5 rounded-full bg-slate-200 w-fit dark:bg-[#3b3b3b]'>
-							<div className='flex items-center'>
-								<IoPodiumOutline
-									className='mr-1 -ml-1'
-									size={16}
-									strokeWidth={20}
-								/>
-								<span className='font-semibold'>
-									{addOrdinalSuffix(Number(championship_pos))}
-								</span>
-							</div>
+					<div className='px-4 py-1 rounded-full bg-slate-200 w-fit dark:bg-[#3b3b3b]'>
+						<div className='flex items-center'>
+							<Trophy
+								className='mr-1 -ml-1'
+								size={16}
+								strokeWidth={2.0}
+							/>
+							<span className='font-semibold'>
+								{points.toLocaleString()}
+							</span>
 						</div>
-						<div className='px-4 py-1 ml-3 mt-5 rounded-full bg-slate-200 w-fit dark:bg-[#3b3b3b]'>
-							<div className='flex items-center'>
-								<Trophy
-									className='mr-1 -ml-1'
-									size={16}
-									strokeWidth={2.0}
-								/>
-								<span className='font-semibold'>
-									{points.toLocaleString()}
-								</span>
-							</div>
-						</div>
-						<div className='px-4 py-1 ml-3 mt-5 rounded-full bg-slate-200 w-fit dark:bg-[#3b3b3b]'>
-							<div className='flex items-center'>
-								<LayoutList
-									className='mr-1 -ml-1'
-									size={16}
-									strokeWidth={2.0}
-								/>
-								<span className='font-semibold'>
-									{category.toUpperCase()}
-								</span>
-							</div>
+					</div>
+					<div className='px-4 py-1 rounded-full bg-slate-200 w-fit dark:bg-[#3b3b3b]'>
+						<div className='flex items-center'>
+							<LayoutList
+								className='mr-1 -ml-1'
+								size={16}
+								strokeWidth={2.0}
+							/>
+							<span className='font-semibold'>
+								{category.toUpperCase()}
+							</span>
 						</div>
 					</div>
 				</div>
-			</div>
-		</>
+			</CardContent>
+		</Card>
 	);
 }
